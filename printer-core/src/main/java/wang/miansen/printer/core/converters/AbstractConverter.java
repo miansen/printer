5c95920d8ca2d8899fa497c1d540dafe1e453b67
MianSenWang <1158827539@qq.com>
2020-04-12 23:31:18 +0800

    add AbstractConverter

---
+++ printer-core/src/main/java/wang/miansen/printer/core/converters/AbstractConverter.java
@@ -0,0 +1,18 @@
+package wang.miansen.printer.core.converters;
+
+/**
+ * @author miansen.wang
+ * @date 2020-04-10
+ */
+public class AbstractConverter implements Converter {
+
+	/* (non-Javadoc)
+	 * @see wang.miansen.printer.core.converters.Converter#convert(java.lang.Object, java.lang.Class)
+	 */
+	@Override
+	public <T> T convert(Object value, Class<T> type) throws ConversionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
